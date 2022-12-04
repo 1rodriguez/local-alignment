913af923300d107943634e067c66833e485aced9
Juan Rodriguez <juanrodriguez@Juans-MacBook-Air.local>
2022-12-04 11:07:28 -0500

    format with black

--- seq-match.py
+++ seq-match.py
@@ -1,13 +1,16 @@
 from numpy import zeros, empty
 
 
-
 def sa(x: str, y: str, s: tuple):
     d = (len(y) + 1, len(x) + 1)
-    
-    F = zeros(d)
 
-    ms, mms, gap_open, gap_extend = s # match, mismatch, gap open (d), and gap extend (e) from score tuple
+    F = zeros(d)
+    (
+        ms,
+        mms,
+        gap_open,
+        gap_extend,
+    ) = s  # match, mismatch, gap open (d), and gap extend (e) from score tuple
     # NOTE: mms and gs should be passed as negative values
 
     """
@@ -17,51 +20,46 @@ def sa(x: str, y: str, s: tuple):
 
     NOTE: No longer true for affine
     """
-    P = empty(d, dtype=tuple) # Keep track of pointers
+    P = empty(d, dtype=tuple)  # Keep track of pointers
 
     # 'match' is used to determine whether to add/subtract the score for a match/mismatch depending on the two letters presently compared
     match: bool
 
     # used to find the global max of the matrix the, where we will work backwards to determine the xy, for the optimal path
     globalMax = 0
-    globalMaxXY: tuple = (1,1)
+    globalMaxXY: tuple = (1, 1)
 
-
-    # Algorithm starts at [1, 1]  as top/left rows were previously intiialized    
+    # Algorithm starts at [1, 1]  as top/left rows were previously intiialized
     for i in range(1, d[0]):
         for j in range(1, d[1]):
             match = x[j - 1] == y[i - 1]
 
             score = ms if match else mms
-            
+
             # a formula: gamma(g) = -d -(g-1)e, affine gap penalty
             def gap_pen(g):
                 return -gap_open - (g - 1) * gap_extend
 
             # We don't mind true maximum values being < 0 as these values will be mapped to 0 by the lambda on score array 'r' later
-            row_max: int # first loop
+            row_max: int  # first loop
             rm_loc: int
 
-            col_max: int # second loop
-            cm_loc: int 
-            
+            col_max: int  # second loop
+            cm_loc: int
+
             for k in range(0, i):
                 val = F[k][j] + gap_pen(i - k)
-                if row_max is None or val > row_max: 
+                if row_max is None or val > row_max:
                     row_max = val
                     rm_loc = k
 
             for k in range(0, j):
                 val = F[i][k] + gap_pen(j - k)
-                if col_max is None or val > col_max: 
+                if col_max is None or val > col_max:
                     col_max = val
                     cm_loc = k
 
-            r = [
-                F[i - 1][j - 1] + score,
-                row_max,
-                col_max
-                ]
+            r = [F[i - 1][j - 1] + score, row_max, col_max]
 
             """
             The minimum values in the array per the Smith-Waterman algorithm are 0 (e.g. no penalty for starting string-matching at arbitrary points in the pair of strings)
@@ -75,7 +73,7 @@ def sa(x: str, y: str, s: tuple):
             pointers: tuple = ()
 
             for k, e in enumerate(r):
-                """ 
+                """
                 If we use row_max, put (rm_loc, j) into the tuple,
                 otherwise, if we use col_max, put (i, cm_loc) into the tuple
 
@@ -86,12 +84,12 @@ def sa(x: str, y: str, s: tuple):
                 |val| = the index along the row or column that we're pointing to
                 """
                 if e == m:
-                    if (k == 0):
+                    if k == 0:
                         pointers += (k,)
-                    elif (k == 1): # up, column
+                    elif k == 1:  # up, column
                         pointers += (cm_loc,)
-                        pass   
-                    else: # k == 2, left, row
+                        pass
+                    else:  # k == 2, left, row
                         pointers += (-rm_loc,)
 
             F[i][j] = m
@@ -101,7 +99,8 @@ def sa(x: str, y: str, s: tuple):
             if globalMax < m:
                 globalMax = m
                 globalMaxXY = (i, j)
-    return (traceback(x, y, F, P, globalMaxXY))
+    return traceback(x, y, F, P, globalMaxXY)
+
 
 ## col string = the one on the top
 ## row string = the one on the left axis
@@ -118,21 +117,21 @@ def traceback(colString: str, rowString: str, matrix, ptrMatrix, globalMaxXY: tu
     while currMax != 0:
         recordOfCurMaxXY.insert(0, (x, y))
         currPtr = ptrMatrix[x][y]
-        #this block came from the up-left position
+        # this block came from the up-left position
         if currPtr[0] == 0:
             x = x - 1
             y = y - 1
             currMax = matrix[x][y]
-        #this block came from the up position
+        # this block came from the up position
         elif currPtr[0] == 1:
             x = x - 1
             currMax = matrix[x][y]
-        #this block came from the left position
+        # this block came from the left position
         else:
             y = y - 1
             currMax = matrix[x][y]
 
-    #once the optimal xy route is found, create the strings using the loop below
+    # once the optimal xy route is found, create the strings using the loop below
     lastx = -1
     lasty = -1
     for i in range(0, len(recordOfCurMaxXY)):
@@ -149,7 +148,7 @@ def traceback(colString: str, rowString: str, matrix, ptrMatrix, globalMaxXY: tu
         lastx = tuple[0]
         lasty = tuple[1]
 
-    return(s1, s2)
+    return (s1, s2)
 
 
 print(sa("AATTTTATGTA", "GATACTTG", (2, -1, -1)))
