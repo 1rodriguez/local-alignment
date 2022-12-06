414894f5f64cb295b1846573cfc5ab8900e64d68
Juan Rodriguez <juanrodriguez@Juans-MacBook-Air.local>
2022-12-05 23:14:07 -0500

    fix gitignore yet again

--- walkthrough/.next/static/chunks/webpack.js
+++ walkthrough/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b652c830b204dc6c"; }
+/******/ 		__webpack_require__.h = function() { return "89ac541b4c1a8007"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
